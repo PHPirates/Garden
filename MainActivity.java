ae2a9e0f1786cd7449f9c1a0da114263f2d3c1f7
PHPirates <hollandpirates@gmail.com>
2015-11-06 16:18:31 +0100

    Oops, I have to upload the right class...

--- MainActivity.java
+++ MainActivity.java
@@ -1,70 +1,146 @@
 package com.garden.gardenapp;
 
-import android.app.Activity;
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RemoteViews;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class DisplayNotification extends Activity {
-    //called when activity is first created
-    //don't forget to update the manifest!!
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        //get notification ID passed by MainActivity
-        int notifID = getIntent().getExtras().getInt("NotifID");
-        //----Oh of course, have to pass on the strings again.....-----
-        //initialize strings (reminder is also used for notification text
-        String reminder = getIntent().getStringExtra("notification");
-        String snoozeString = getIntent().getStringExtra("notifyAction");
+        //right bottom floating button
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+/* //for normal buttons
+    public void settings(View v) { //onclick
+        Toast.makeText(this,"button clicked",Toast.LENGTH_LONG).show();
+    }
+*/
+
+    public void setAlarm(View v) {
+        TimePicker timePicker; //TODO the pickers should be in a DialogFragment
+        DatePicker datePicker;
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        datePicker = (DatePicker) findViewById(R.id.datePicker);
+        //get user input
+        EditText editText = (EditText) findViewById(R.id.editReminder);
+        String reminder = editText.getText().toString();
+        String snoozeString = getString(R.string.snooze_result);
+
+        //the AlarmManager
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        //get date and time
+        Calendar c = Calendar.getInstance();
+
+        //sets time for alarm
+        c.set(Calendar.YEAR, datePicker.getYear());
+        c.set(Calendar.MONTH, datePicker.getMonth());
+        c.set(Calendar.DAY_OF_MONTH, datePicker.getDayOfMonth());
+        c.set(Calendar.HOUR_OF_DAY, timePicker.getCurrentHour()); //getHour requires 23...
+        c.set(Calendar.MINUTE, timePicker.getCurrentMinute());
+        c.set(Calendar.SECOND, 0);
+
+        //pIntent to launch activity when alarm triggers
+        Intent intent = new Intent("com.garden.DisplayNotification"); //(1)From here to DisplayNotification ...
+        //DisplayNotification is the activity that is intended to be evoked
+        //sometimes you have to use (this,DisplayNotification.class)?
+        //when the alarm is triggered
+
+        //assign an ID of 1, and add the text
+        intent.putExtra("NotifID", 1);
+        intent.putExtra("notification", reminder); //("STRING_I_NEED",strnamed)
+        intent.putExtra("notifyAction", snoozeString); //string for the action button
+
+        PendingIntent displayIntent = PendingIntent.getActivity(
+                getBaseContext(), 0,
+                intent, 0); //this intent instead of new Intent("com.garden.Reminder")
 
 
+        //sets alarm
+        alarmManager.set(AlarmManager.RTC_WAKEUP,
+                c.getTimeInMillis(), displayIntent);
 
-        //pIntent to launch activity if user selects notification
-        /** making a new Intent has to be done with (this, Reminder.class) instead
-         * of ("com.garden.Reminder")... why? (otherwise the reminder text is not shown)
+
+    }
+
+    public void setAlarmNow (View view) {
+        /**
+         * This is a debugging method that is the same as setAlarm but
+         * sends a notification instantly.
          */
-        Intent intent = new Intent(this, Reminder.class); //(2)... and from here to Reminder ...
-        intent.putExtra("NotifID",notifID);
-        //pass on strings again in the intent
-        intent.putExtra("notification",reminder);
-        //intent.putExtra("notifyAction",snoozeString); //---> in different intent
+        TextView textView = (TextView) findViewById(R.id.hello);
+        EditText editText = (EditText) findViewById(R.id.editReminder);
+        String reminder = editText.getText().toString();
+        textView.setText(reminder);
+        String snoozeString = "I'm snoozed";
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        Intent intent = new Intent("com.garden.DisplayNotification");
+        intent.putExtra("NotifID", 1);
+        intent.putExtra("notification", reminder);
+        intent.putExtra("notifyAction", snoozeString);
+        PendingIntent displayIntent = PendingIntent.getActivity(
+                getBaseContext(), 0,
+                intent, 0);
+        alarmManager.set(AlarmManager.RTC_WAKEUP,
+                System.currentTimeMillis(), displayIntent);
+    }
 
-        PendingIntent detailsIntent = PendingIntent
-                .getActivity(this, 0, intent, 0);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
-        Intent actionIntent = new Intent(this, Reminder.class);
-        actionIntent.putExtra("notifyAction", snoozeString); //("STRING_I_NEED", strName)
-        //don't forget to pass the notifID also to the second intent
-        actionIntent.putExtra("NotifID",notifID);
-        PendingIntent actionpIntent = PendingIntent.getActivity(this,
-                (int) System.currentTimeMillis(), actionIntent, 0);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
 
+        //what happens when you click what button (in the action bar)
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Toast.makeText(this, "There are no settings yet!", Toast.LENGTH_LONG).show();
+            case R.id.action_about:
+                Toast.makeText(this, "About nothing, really.", Toast.LENGTH_LONG).show();
+                TextView textView = (TextView) findViewById(R.id.hello);
+                textView.setText(getString(R.string.newText));
 
-        //create notification
-        Notification notif = new Notification.Builder(this) //build the notification
-                .setContentTitle(getString(R.string.app_name)) //required
-                .setContentText(reminder) //required
-                .setSmallIcon(R.drawable.garden) //required
-                .setContentIntent(detailsIntent)
-                //associate pendingIntent with a gesture of NotificationCompat.Builder: click
-                .addAction(R.drawable.pixel, "Snooze me", actionpIntent)
-                //should be addAction(NotificationCompat.Action action)
-                .setAutoCancel(true) //to be dismissed in the Reminder activity
-                .setPriority(Notification.PRIORITY_MAX) //to show the action buttons by default
-                // .setVibrate(new long[] {200, 600, 200, 600})
-                .build();
+        }
 
-        NotificationManager nm = (NotificationManager)
-                getSystemService(NOTIFICATION_SERVICE);
-
-        nm.notify(notifID, notif); //(int id, Notification notification);
-
-        finish(); //because this doesn't have a GUI we don't need it anymore
-
+        return super.onOptionsItemSelected(item);
     }
 }
