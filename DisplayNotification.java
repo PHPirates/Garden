79aaad07f9716a80715c3038e3227740a9219337
PHPirates <hollandpirates@gmail.com>
2015-11-07 21:19:18 +0100

    Update DisplayNotification.java

--- DisplayNotification.java
+++ DisplayNotification.java
@@ -8,9 +8,14 @@ import android.content.Intent;
 import android.os.Bundle;
 
 public class DisplayNotification extends Activity {
+    /**
+     * This activity is to display a notification only. It is called when
+     * the system alarm goes off.
+     * @param savedInstanceState What is a bundle?
+     */
+
     //called when activity is first created
     //don't forget to update the manifest!!
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,47 +24,50 @@ public class DisplayNotification extends Activity {
         int notifID = getIntent().getExtras().getInt("NotifID");
         //----Oh of course, have to pass on the strings again.....-----
         //initialize strings (reminder is also used for notification text
-        String reminder = getIntent().getStringExtra("notification");
+        String reminderText = getIntent().getStringExtra("notification");
         String snoozeString = getIntent().getStringExtra("notifyAction");
-
+        System.out.println("disp"+reminderText);
 
 
         //pIntent to launch activity if user selects notification
         /** making a new Intent has to be done with (this, Reminder.class) instead
          * of ("com.garden.Reminder")... why? (otherwise the reminder text is not shown)
          */
-        Intent intent = new Intent(this, Reminder.class); //(2)... and from here to Reminder ...
-        intent.putExtra("NotifID",notifID);
+        Intent reminderIntent = new Intent(this, Reminder.class); //(2)... and from here to Reminder ...
+        reminderIntent.putExtra("NotifID", notifID);
         //pass on strings again in the intent
-        intent.putExtra("notification",reminder);
+        reminderIntent.putExtra("notification", reminderText);
         //intent.putExtra("notifyAction",snoozeString); //---> in different intent
 
-        PendingIntent detailsIntent = PendingIntent
-                .getActivity(this, 0, intent, 0);
+        PendingIntent reminderPIntent = PendingIntent
+                .getActivity(this, 0, reminderIntent, 0);
 
         Intent actionIntent = new Intent(this, Reminder.class);
         actionIntent.putExtra("notifyAction", snoozeString); //("STRING_I_NEED", strName)
         //don't forget to pass the notifID also to the second intent
         actionIntent.putExtra("NotifID",notifID);
-        PendingIntent actionpIntent = PendingIntent.getActivity(this,
+        PendingIntent actionPIntent = PendingIntent.getActivity(this,
                 (int) System.currentTimeMillis(), actionIntent, 0);
 
 
         //create notification
         Notification notif = new Notification.Builder(this) //build the notification
                 .setContentTitle(getString(R.string.app_name)) //required
-                .setContentText(reminder) //required
+                .setContentText(reminderText) //required
                 .setSmallIcon(R.drawable.garden) //required
-                .setContentIntent(detailsIntent) //to direct the user to an activity?
-                .addAction(R.drawable.pixel, "Snooze me", actionpIntent)
+                .setContentIntent(reminderPIntent)
+                //associate pendingIntent with a gesture of NotificationCompat.Builder: click
+                .addAction(R.drawable.pixel, "Snooze me", actionPIntent)
+                //should be addAction(NotificationCompat.Action action)
                 .setAutoCancel(true) //to be dismissed in the Reminder activity
                 .setPriority(Notification.PRIORITY_MAX) //to show the action buttons by default
+                // .setVibrate(new long[] {200, 600, 200, 600})
                 .build();
 
         NotificationManager nm = (NotificationManager)
                 getSystemService(NOTIFICATION_SERVICE);
 
-        nm.notify(notifID, notif);
+        nm.notify(notifID, notif); //(int id, Notification notification);
 
         finish(); //because this doesn't have a GUI we don't need it anymore
 
