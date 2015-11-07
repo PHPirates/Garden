04cefaaf62d1d3b96294c64c4fdd0ff6fb522d62
PHPirates <hollandpirates@gmail.com>
2015-11-07 21:19:50 +0100

    Update Reminder.java

--- Reminder.java
+++ Reminder.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
 
@@ -36,21 +37,25 @@ public class Reminder extends AppCompatActivity {
 
 
         //the textview that shows the string the notification passed
-        TextView textView = (TextView) findViewById(R.id.reminderText);
-        String notifyText = getIntent().getStringExtra("notification");
-        textView.setText(notifyText);
-        //now the same for the action button from the notification
-        TextView snoozeView = (TextView) findViewById(R.id.snoozeText);
-        //(3).... and then get String and setText!
-        String actionText = getIntent().getStringExtra("notifyAction"); //getString("STRING_I_NEED");
-        snoozeView.setText(actionText);
+        TextView mainTextView = (TextView) findViewById(R.id.reminderText);
+        String reminderText = getIntent().getStringExtra("notification");
+        System.out.println("reminder" + reminderText); //TODO string always empty
 
+        //now the same for the action button from the notification
+        TextView snoozeTextView = (TextView) findViewById(R.id.snoozeText);
+        //(3).... and then get String and setText!
+        String snoozeText = getIntent().getStringExtra("notifyAction"); //getString("STRING_I_NEED");
+        if (TextUtils.isEmpty(reminderText)) {
+            //if the reminder string is empty it means the snooze button was pressed
+            snoozeTextView.setText(snoozeText);
+        } else {
+            mainTextView.setText(reminderText);
+        }
         //This will remove the notification if the action button is pressed
         // instead of the notification, but only when the activity is displayed
         NotificationManager nm = (NotificationManager)
                 getSystemService(NOTIFICATION_SERVICE);
         nm.cancel(getIntent().getExtras().getInt("NotifID"));
-        //TODO dismiss notification when snooze is clicked
     }
 
 }
